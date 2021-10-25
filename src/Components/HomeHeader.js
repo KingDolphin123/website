6d643e96401194cf79fc00fe7214ae49db892a29
KingDolphin123 <jleem2b@gmail.com>
2021-10-25 16:47:02 -0400

    vb

--- src/Components/HomeHeader.js
+++ src/Components/HomeHeader.js
@@ -2,23 +2,28 @@ import React from "react";
 import '../css/HomeHeader.css';
 import arrow from '../css/arraw.png';
 
- 
-const Header = () => {
 
+const Header = () => {
+    const scrollOne = () => {
+        window.scrollTo({
+            top: 1000,
+            behavior: 'smooth'
+        })
+    }
     return (
-        <div className = 'headerpage'>
+        <div className='headerpage'>
             <div className="Welcome">
-                <p className = "letter W">W</p>
-                <p className = "letter E">E</p>
-                <p className = "letter L">L</p>
-                <p className = "letter C">C</p>
-                <p className = "letter O">O</p>
-                <p className = "letter M">M</p>
-                <p className = "letter E2">E</p>
+                <p className="letter W">W</p>
+                <p className="letter E">E</p>
+                <p className="letter L">L</p>
+                <p className="letter C">C</p>
+                <p className="letter O">O</p>
+                <p className="letter M">M</p>
+                <p className="letter E2">E</p>
             </div>
-            <input className = "downarrow" type = "image" src={arrow} alt = 'arrowdown' />
+            <input className="downarrow" type="image" src={arrow} alt='arrowdown' onClick={scrollOne} />
         </div>
     )
 }
- 
+
 export default Header;
