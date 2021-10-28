6818cde3f996d2abfae898da2a57f52686c38c61
KingDolphin123 <jleem2b@gmail.com>
2021-10-28 17:54:27 -0400

    para

--- src/Components/AboutBody.js
+++ src/Components/AboutBody.js
@@ -6,6 +6,8 @@ import mnt3 from '../css/mnt3.png'
 import cld1 from '../css/cld1.png'
 import cld2 from '../css/cld2.png'
 import cld3 from '../css/cld3.png'
+import rect from '../css/rect.png'
+
 
 
 
@@ -27,6 +29,7 @@ const Para = () => {
             <img className='ree' src={mnt1} alt='mnt1' style={{ transition: `.35s`, transform: `translateY(-${offsetY * 1.1}px)` }}></img>
             <img className='ree' src={cld2} alt='cld2' style={{ transition: `.35s`, transform: `translateY(-${offsetY * 1.3}px)` }}></img>
             <img className='ree' src={cld1} alt='cld1' style={{ transition: `.35s`, transform: `translateY(-${offsetY * 1.5}px)` }}></img>
+            <img className = 'rect' src={rect} alt='rect' style={{ transition: `.35s`, transform: `translateY(-${offsetY * 1.5}px)` }}></img>
         </div>
     )
 }
